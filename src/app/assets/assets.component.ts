1cde5577607a68bbc559a6c4fe27ead6f8e3eab4
EloWord <elowordFr@gmail.com>
2024-01-27 22:33:11 +0100

    feat: Add Assets Component with Associated Functionalities
    - Created the Assets component to display held assets.
    - Integrated mock data for the initial display of assets.
    - Added a refresh button to update the asset data.
    - Implemented a send button that triggers the display of the asset send form.
    - Created the send form with necessary fields for destination address, amount, execution tick, and asset selection.
    - Established data coherence checks to ensure the validity of data entered in the send form, including validation of the amount range based on the selected asset.
    - Added a cancellation button to close the send form without submitting data.
    - Added a submit button to send data from the send form.
    
    These enhancements provide a comprehensive user interface for managing assets, including viewing, refreshing data, and sending assets.

--- src/app/assets/assets.component.ts
+++ src/app/assets/assets.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {QubicAsset} from "../services/api.model";
+import {ApiService} from "../services/api.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-assets',
   templateUrl: './assets.component.html',
   styleUrls: ['./assets.component.scss']
 })
-export class AssetsComponent {
+
+export class AssetsComponent implements OnInit {
+  displayedColumns: string[] = ['publicId', 'contractIndex', 'assetName', 'contractName', 'ownedAmount', 'possessedAmount', 'tick', 'reportingNodes'];
+  public assets: QubicAsset[] = [];
+
+  sendForm: FormGroup;
+  showSendForm: boolean = false;
+  balanceTooLow: boolean = false; // logic
+
+
+  constructor(private apiService: ApiService) {
+    this.sendForm = new FormGroup({
+      destinationAddress: new FormControl('', Validators.required),
+      amount: new FormControl('', Validators.required),
+      tick: new FormControl('', Validators.required),
+      assetSelect: new FormControl('', Validators.required),
+    });
+
+    // Sécuriser les souscriptions avec des vérifications
+    const amountControl = this.sendForm.get('amount');
+    const assetSelectControl = this.sendForm.get('assetSelect');
+
+    if (amountControl) {
+      amountControl.valueChanges.subscribe(() => {
+        this.updateAmountValidator();
+      });
+    }
+
+    if (assetSelectControl) {
+      assetSelectControl.valueChanges.subscribe(() => {
+        this.updateAmountValidator();
+      });
+    }
+  }
+
+  updateAmountValidator(): void {
+    const assetSelectControl = this.sendForm.get('assetSelect');
+    const amountControl = this.sendForm.get('amount');
+
+    if (assetSelectControl && amountControl) {
+      const selectedAsset = assetSelectControl.value;
+      const maxAmount = selectedAsset ? selectedAsset.ownedAmount : 0;
+
+      amountControl.setValidators([
+        Validators.required,
+        Validators.min(0),
+        Validators.max(maxAmount)
+      ]);
+
+      amountControl.updateValueAndValidity();
+    }
+  }
+
+  ngOnInit() {
+    this.loadAssets();
+  }
+
+  refreshData(): void {
+     this.loadAssets();
+  }
+
+  protected loadAssets(): void {
+    // TODO replace
+    const publicIds = ['id1', 'id2'];
+
+    this.apiService.getOwnedAssetsM(publicIds).subscribe(
+      (assets: QubicAsset[]) => {
+        this.assets = assets;
+      },
+      error => {
+        console.error('Error when loading assets', error);
+      }
+    );
+  }
+
+  openSendForm(): void {
+    this.showSendForm = true;
+    const assetSelectControl = this.sendForm.get('assetSelect');
+    if (assetSelectControl && this.assets.length > 0) {
+      assetSelectControl.setValue(this.assets[0]);
+      this.updateAmountValidator();
+    }
+  }
+
+  onSubmitSendForm(): void {
+    if (this.sendForm.valid) {
+      // logic
+    }
+  }
+
+  cancelSendForm(): void {
+    this.showSendForm = false;
+    this.sendForm.reset();
+  }
+
 
 }
