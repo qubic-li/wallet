f8d864e45f79608d4b6c3016c7ebd4cc5af9efde
AndyQus <AndyQus@outlook.com>
2024-02-04 01:45:42 +0100

    website is a PWA now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -71,6 +71,7 @@ import { AssetsDialog } from './main/assets/assets.component';
 import {MatMenuModule} from "@angular/material/menu";
 import { AssetsComponent } from './assets/assets.component';
 import { TransactionService } from './services/transaction.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 
@@ -144,7 +145,13 @@ export const httpInterceptorProviders = [
     MatStepperModule,
     MatProgressSpinnerModule,
     MatSlideToggleModule,
-    MatMenuModule
+    MatMenuModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
 
   providers: [
